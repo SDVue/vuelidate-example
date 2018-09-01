754b2e3f84f433948ea284f8cafbddde6058e123
Austin Gil <austin@stegosource.com>
2018-08-31 21:45:36 -0700

    Adding Loops

---
+++ src/examples/6-Loops.vue
@@ -0,0 +1,56 @@
+<template>
+  <form @submit.prevent="$v.$touch()">
+    <fieldset>
+      <legend>Name 3 heroes:</legend>
+
+      <label v-for="(hero, index) in heroes" :key="hero.id">{{ index + 1 }}:
+        <input v-model="hero.name">
+
+        <div v-if="$v.heroes.$each[index].name.$error" class="validation-errors">
+          <small v-if="!$v.heroes.$each[index].name.required">Field is required.</small>
+          <small v-if="!$v.heroes.$each[index].name.min">Must be at least {{ $v.heroes.$each[index].name.$params.min.min }} characters.</small>
+        </div>
+
+        <!-- <pre>{{ $v.heroes.$each[index] }}</pre> -->
+      </label>
+    </fieldset>
+
+    <button>Submit</button>
+
+    <!-- <pre v-for="hero in heroes" :key="hero.id">{{ hero }}</pre>
+    <hr>
+    <pre v-for="hero in $v.heroes.$each.$iter" :key="hero.id">{{ hero }}</pre> -->
+  </form>
+</template>
+
+<script>
+import { required, minLength } from "vuelidate/lib/validators";
+
+const heroes = new Array(3).fill(0).map((el, id) => ({
+  id,
+  name: ""
+}));
+
+export default {
+  data: () => ({
+    heroes
+  }),
+
+  validations: {
+    heroes: {
+      $each: {
+        name: {
+          required,
+          min: minLength(3)
+        }
+      }
+    }
+  }
+};
+</script>
+
+<style lang="scss">
+fieldset > div {
+  margin-bottom: 30px;
+}
+</style>
